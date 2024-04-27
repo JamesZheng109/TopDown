4887453ef78c2507c20bddc92e9e96416978fccb
James Zheng <zhengjames35@gmail.com>
2024-04-27 06:27:16 -0400

    Update Wrapper.py

--- Scripts/Wrapper.py
+++ Scripts/Wrapper.py
@@ -1,5 +1,4 @@
 from Text import speech
-click='..\\Audio\\SFX\\Click.wav'
 def textwrapper(window,speaker,dialogue,image=[],background=None,sound=[],default_sfx=click):
     speech(window=window,name=speaker,namefontsize=50,text=dialogue,
                    textfontsize=50,text_color='black',box_color='yellow',box_x=100,box_y=700,width=1300,height=220,
